b0d4e9b5cd6acfc0f1b6bc0093b9e3413083ec64
Dinesh Panda <dineshpanda92@gmail.com>
2020-08-06 19:03:37 +0530

    Whitelist columns in Blog for ActiveAdmin

--- app/admin/blogs.rb
+++ app/admin/blogs.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Blog do
 
+ permit_params :content, :image, :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
